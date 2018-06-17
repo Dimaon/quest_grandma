e4567499cf663426b23d69875bc08027b0733ad5
Dimaon <kulikov.perm@gmail.com>
2018-06-17 17:04:55 +0300

    Transfer to private methods

--- lib/scenes/street.rb
+++ lib/scenes/street.rb
@@ -17,33 +17,38 @@ def street_logic
 
   if choice == 2
     puts "Амбар заперт на замок."
-    if $inventory.include? "ключ"
-      variants = ["открыть замок ключом", "выйти во двор"]
+    ambar_logic
+  else
+    main_process
+  end
+end
+
+private
+
+def ambar_logic
+  if $inventory.include? "ключ"
+    variants = ["открыть замок ключом", "выйти во двор"]
+    choice = puts_variants(variants)
+
+    if choice == 1
+      puts "Ты зашел в амбар. В амбаре старый культиватор."
+      buffer
+
+      variants = ["поискать топор", "закрыть амбар и выйти во двор"]
       choice = puts_variants(variants)
 
       if choice == 1
-        puts "Ты зашел в амбар. В амбаре старый культиватор."
-        buffer
+        puts "+++ Ты нашел топор."
+        $inventory << "топор"
 
-        variants = ["поискать топор", "закрыть амбар и выйти во двор"]
-        choice = puts_variants(variants)
-
-        if choice == 1
-          puts "+++ Ты нашел топор."
-          $inventory << "топор"
-          buffer
-
-          street_logic if get_answer?("1. закрыть амбар и выйти во двор")
-        else
-          street_logic
-        end
+        street_logic if get_answer?("1. закрыть амбар и выйти во двор")
       else
         street_logic
       end
     else
-      street_logic if get_answer?("1. выйти во двор")
+      street_logic
     end
   else
-    main_process
+    street_logic if get_answer?("1. выйти во двор")
   end
 end
--- lib/scenes/table.rb
+++ lib/scenes/table.rb
@@ -1,3 +1,10 @@
+# Рядом со столом
+def table_logic
+  puts "Обычное окно и рядом стол. На нем ваза цветов. Тебе они могут понадобиться на похороны."
+  close_window
+end
+
+private
 # Закрыть окно
 def close_window
   variants = ["Закрыть окно", "Осмотреться"]
@@ -26,10 +33,4 @@ def open_window
   else
     main_process
   end
-end
-
-# Рядом со столом
-def table_logic
-  puts "Обычное окно и рядом стол. На нем ваза цветов. Тебе они могут понадобиться на похороны."
-  close_window
 end
